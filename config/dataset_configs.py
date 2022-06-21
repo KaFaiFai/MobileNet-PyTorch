ac167f1d9c9378dd9b78ed0d529cd9ade5a16e2c
Cyrus <cyrus.cheungkf@gmail.com>
2022-06-21 18:05:26 +0800

    + configs for model and dataset definition and path

---
+++ config/dataset_configs.py
@@ -0,0 +1,21 @@
+from dataset import *
+
+DATASETS = {
+    "stanford-dogs": {
+        "class": DogsDataset,
+        "train_root": r"D:\_Dataset\Stanford Dogs",
+        "test_root": r"D:\_Dataset\Stanford Dogs",
+    }, "mnist": {
+        "class": MNISTDataset,
+        "train_root": r"D:\_Dataset",
+        "test_root": r"D:\_Dataset",
+    }, "cifar10": {
+        "class": CIFAR10Dataset,
+        "train_root": r"D:\_Dataset\CIFAR10",
+        "test_root": r"D:\_Dataset\CIFAR10",
+    }, "imagenet": {
+        "class": ImageNetDataset,
+        "train_root": r"D:\_Dataset\ImageNet_2012",
+        "test_root": r"D:\_Dataset\ImageNet_2012",
+    },
+}
