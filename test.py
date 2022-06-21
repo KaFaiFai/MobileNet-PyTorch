c5c36da8ded09e4b8e73cdafeaf40545dbaa86e1
Cyrus <cyrus.cheungkf@gmail.com>
2022-06-21 23:24:34 +0800

    + support cli arguments for test.py

--- test.py
+++ test.py
@@ -7,61 +7,65 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import torchvision.models as models
-from collections import defaultdict
 
-from model import *
-from dataset import *
+from collections import defaultdict
+import argparse
+from pathlib import Path
+
+from config import *
 from script import test_loop
 from script.utils import be_deterministic
 
 be_deterministic()
 
+parser = argparse.ArgumentParser()
+# required settings
+parser.add_argument("--data", type=str, help="dataset", choices=DATASETS.keys(), required=True)
+parser.add_argument("--model", type=str, help="model type", choices=MODELS.keys(), required=True)
+parser.add_argument("--pretrained-model", type=Path, help="path to pretrained model", required=True)
+parser.add_argument("--batch-size", type=int, help="training batch size", required=True)
 
-def test():
+# misc parameters
+parser.add_argument("--device", type=str, help="cpu or gpu?", choices=["cpu", "cuda"], default="cuda")
+parser.add_argument("--num-workers", type=int, help="sub-processes for data loading", default=0)
+
+# misc settings
+parser.add_argument("--print-step", type=int, help="How often to print progress (in batch)?")
+
+
+def test(configs):
     # hyper parameters
-    batch_size = 64
+    batch_size = configs.batch_size
 
     # experiment settings
-    data = "imagenet"  # ["stanford-dogs", "mnist", "cifar10", "imagenet"]
-    model_type = "mobile_net"  # ["mobile_net", "lenet"]
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    num_workers = 4
-    pretrained_model_path = r"C:\_Project\Pycharm Projects\MobileNet\pretrained\wjc-mobilenet-a100-r224-c1000-e0000.pth"
+    data = configs.data
+    model_type = configs.model
+    device = configs.device if torch.cuda.is_available() else "cpu"
+    num_workers = configs.num_workers
+    pretrained_model_path = configs.pretrained_model
 
-    assert data in ["dogs", "mnist", "cifar10", "imagenet"]
-    assert model_type in ["mobile_net", "lenet"]
+    assert data in DATASETS.keys()
+    assert model_type in MODELS.keys()
 
     # training configs
     c = defaultdict(lambda: None)
-    c["print_step_test"] = 20
+    c["print_step_test"] = configs.print_step
     c["device"] = device
 
     # select dataset
-    test_dataset = None
-    if data == "stanford-dogs":
-        test_dataset = DogsDataset(root=r"D:\_Dataset\Stanford Dogs", is_train=False)
-    elif data == "mnist":
-        test_dataset = MNISTDataset(root=r"D:\_Dataset", is_train=False)
-    elif data == "cifar10":
-        test_dataset = CIFAR10Dataset(root=r"D:\_Dataset\CIFAR10", is_train=False)
-    elif data == "imagenet":
-        test_dataset = ImageNetDataset(root=r"D:\_Dataset\ImageNet_2012", is_train=False)
+    Dataset = DATASETS[data]["class"]
+    test_root = DATASETS[data]["test_root"]
+    test_dataset = Dataset(root=test_root, is_train=False)
     num_class = test_dataset.num_labels
 
     # set up model, dataloader, optimizer, criterion
-    network = None
-    if model_type == "lenet":
-        network = LeNet(num_class).to(device)
-    elif model_type == "mobile_net":
-        network = MobileNet(num_class).to(device)
+    Network = MODELS[model_type]
+    network = Network(num_class).to(device)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
     criterion = nn.CrossEntropyLoss()
 
     state = torch.load(pretrained_model_path)
     network.load_state_dict(state["state_dict"])
-
-    network = models.mobilenet_v2(pretrained=True)
-    network = network.to(device)
 
     # testing loop
     print(f"{'-' * 5} Test result {'-' * 5}")
@@ -69,4 +73,5 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
+    args = parser.parse_args()
+    test(args)
---
+++ test.sh
@@ -0,0 +1,6 @@
+python test.py \
+--data image-net \
+--model mobile-net \
+--pretrained-model .\pretrained\wjc-mobilenet-a100-r224-c1000-e0000.pth \
+--batch-size 64 \
+--print-step 30
